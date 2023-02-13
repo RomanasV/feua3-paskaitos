8a357a6bee632bd914b468d6f1dd688560661ad2
RomanasV <romanasvenckus@gmail.com>
2023-02-13 20:06:05 +0200

    js basics

---
+++ 2023-02-13-js-basics/script.js
@@ -0,0 +1,17 @@
+// Kintamojo sukūrimas:
+// 1. Iniciavimo žodelis (var, let, const)
+// 2. Kintamojo pavadinimas
+// 3. Lygybės ženklas
+// 4. Kintamojo vertė
+
+var vardas = "John"; // String (tekstas) tipo kintamasis
+let pavarde = 'Doe'; // String (tekstas) tipo kintamasis
+
+const asmensKodas = 389879798797;
+
+console.log(vardas);
+console.log(pavarde);
+console.log(asmensKodas);
+
+pavarde = 'Steve';
+console.log(pavarde);
