4b71e467e814068d0482d1369bb978d754bfeff8
RomanasV <romanasvenckus@gmail.com>
2023-02-14 21:25:54 +0200

    password task

--- 2023-02-13-js-basics/script.js
+++ 2023-02-13-js-basics/script.js
@@ -215,3 +215,26 @@ if (age < 0) {
 } else {
   console.log('Amžius per didelis');
 }
+
+let originalPassword = 'aadasasdasdasd      dasdhaskjdh                ';
+let password = originalPassword.trimStart().trimEnd().replaceAll(' ', '');
+
+console.log(password);
+console.log(password.length);
+console.log(password.includes('#'));
+
+// 1. Jeigu slaptažodis yra trumpesnis už 16 simbolių, tai parašyti jog „Slaptažodis yra per trumpas. Jis privalo būti bent 16 simbolių ilgumo."
+// 2. Jeigu slaptažodis yra ilgesnis už 15 simbolių, tai:
+// 2.1. Patikrinti ar jis yra ilgesnis už 20 simbolių. Jeigu ilgesnis, tai parašyti: „Slaptažodis tinkamas".
+// 2.2. Jeigu jis nėra ilgesnis už 20 simbolių, tai parašyti jog: „Slaptažodis yra tinkamas. Tačiau rekomenduojama jog jis būtų bent 21 simbolio ilgumo."
+if (password.length < 16) {
+  console.log('Slaptažodis yra per trumpas. Jis privalo būti bent 16 simbolių ilgumo.');
+} else {
+  if (password.includes('#') === false) {
+    console.log('Slaptažodyje privalo būti grotažymės (#)');
+  } else if (password.length > 20) {
+    console.log('Slaptažodis tinkamas');
+  } else {
+    console.log('Slaptažodis yra tinkamas. Tačiau rekomenduojama jog jis būtų bent 21 simbolio ilgumo.');
+  }
+}
