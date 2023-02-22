b61a196caadd54f614191acfdfd30e886b611339
RomanasV <romanasvenckus@gmail.com>
2023-02-22 19:16:00 +0200

    loop task

--- 2023-02-22-loop/script.js
+++ 2023-02-22-loop/script.js
@@ -66,10 +66,10 @@ function forLoop() {
 //   console.log(i);
 // }
 
-// for (let i = 0; i <= 100; i++) {
-//   console.log(i);
-// }
-
-for (let i = 10; i > 0; i--) {
+for (let i = 1; i <= 100; i++) {
   console.log(i);
 }
+
+// for (let i = 10; i > 0; i--) {
+//   console.log(i);
+// }
