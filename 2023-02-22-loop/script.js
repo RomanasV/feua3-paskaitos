3815ac558826cf1a2995a16e37fe91ae00dfabc1
RomanasV <romanasvenckus@gmail.com>
2023-02-22 19:01:29 +0200

    for loop

---
+++ 2023-02-22-loop/script.js
@@ -0,0 +1,75 @@
+// console.log('-' + 1 + '!');
+// console.log('-' + 2 + '!');
+// console.log('-' + 3 + '!');
+// console.log('-' + 4 + '!');
+// console.log('-' + 5 + '!');
+// console.log('-' + 6 + '!');
+// console.log('-' + 7 + '!');
+// console.log('-' + 8 + '!');
+// console.log('-' + 9 + '!');
+// console.log('-' + 10 + '!');
+// console.log('-' + 11 + '!');
+// console.log('-' + 12 + '!');
+// console.log('-' + 13 + '!');
+// console.log('-' + 14 + '!');
+// console.log('-' + 15 + '!');
+// console.log('-' + 16 + '!');
+// console.log('-' + 17 + '!');
+// console.log('-' + 18 + '!');
+// console.log('-' + 19 + '!');
+// console.log('-' + 20 + '!');
+
+function count(num) {
+  console.log('- ' + num + '!!!');
+}
+
+// count(1);
+// count(2);
+// count(3);
+// count(4);
+// count(5);
+// count(6);
+// count(7);
+// count(9);
+// count(10);
+// count(11);
+// count(12);
+// count(13);
+// count(14);
+// count(15);
+// count(16);
+// count(17);
+// count(19);
+// count(20);
+
+
+// FOR CIKLAS
+
+/*
+
+for (kintamasis; sąlyga; kintamojo pakeitimas) {
+
+}
+ 
+*/
+
+function forLoop() {
+  for (let i = 1; i <= 20; i++) {
+    count(i);
+  }
+}
+
+// forLoop();
+
+
+// for (let i = 0; i < 100; i++) {
+//   console.log(i);
+// }
+
+// for (let i = 0; i <= 100; i++) {
+//   console.log(i);
+// }
+
+for (let i = 10; i > 0; i--) {
+  console.log(i);
+}
