afea333172682b729ed612ad19914a51a7866bc8
RomanasV <romanasvenckus@gmail.com>
2023-03-15 21:24:54 +0200

    cities task

---
+++ 2023-03-08-objects/cities.js
@@ -0,0 +1,115 @@
+let citiesList =  [
+  {
+    name: 'London',
+    population: 11120000,
+    location: {
+      continent: 'Europe',
+      country: 'United Kingdom',
+    },
+    touristAttractions: ['Big Ben'],
+    isCapital: true
+  },
+  {
+    name: 'Paris',
+    population: 11027000,
+    location: {
+      continent: 'Europe',
+      country: 'France',
+    },
+    touristAttractions: [],
+    isCapital: true
+  },
+  {
+    name: 'New York',
+    population: 19294000,
+    location: {
+      continent: 'North America',
+      country: 'United States of America',
+    },
+    touristAttractions: ['Metropolitan Museum', 'Central Park', 'Modern Art Museum'],
+    isCapital: false
+  },
+  {
+    name: 'Dubai',
+    population: 6595000,
+    location: {
+      continent: 'Asia',
+      country: 'United Arab Emirates',
+    },
+    touristAttractions: ['Burj Khalifa', 'Burj Al Arab', 'Dubai Mall'],
+    isCapital: false
+  },
+  {
+    name: 'Tokyo',
+    population: 39105000,
+    location: {
+      continent: 'Asia',
+      country: 'Japan',
+    },
+    touristAttractions: ['Senso-ji', 'Tokyo Skytree', 'Meiji Jingu'],
+    isCapital: true
+  },
+  {
+    name: 'Singapore',
+    population: 5901000,
+    location: {
+      continent: 'Asia',
+      country: 'Singapore',
+    },
+    touristAttractions: ['Marina Bay Sands', 'Gardens by the Bay', 'Sentosa'],
+    isCapital: true
+  },
+  {
+    name: 'Los Angeles',
+    population: 13250000,
+    location: {
+      continent: 'North America',
+      country: 'United States of America',
+    },
+    touristAttractions: ['Museum of Art', 'Disneyland Park', 'Santa Monica Pie'],
+    isCapital: false
+  },
+  {
+    name: 'Barcelona',
+    population: 4735000,
+    location: {
+      continent: 'Europe',
+      country: 'Spain',
+    },
+    touristAttractions: ['La Sagrada Familia', 'Park Guell', 'Casa Batllo'],
+    isCapital: false
+  },
+  {
+    name: 'Rio de Janeiro',
+    population: 6748000,
+    location: {
+      continent: 'South America',
+      country: 'Brazil',
+    },
+    touristAttractions: ['Christ the Redeemer', 'Sugaloaf Mountain', 'Copacabana Beach'],
+    isCapital: false
+  },
+];
+
+
+function renderCities(cities) {
+  console.log(cities);
+  cities.forEach(city => {
+    console.log(city);
+    console.log(city.name);
+    console.log(city.population);
+    console.log(city.isCapital);
+  
+    console.log(city.location);
+    console.log(city.location.continent);
+    console.log(city.location.country);
+  
+    console.log(city.touristAttractions);
+  
+    city.touristAttractions.map(attraction => {
+      console.log(attraction)
+    })
+  });
+}
+
+renderCities(citiesList);
